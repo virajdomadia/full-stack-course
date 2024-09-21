81b10f52acce685d62f049ddf2d387c124d09aaf
Viraj <virajdomadia32@gmail.com>
2024-09-22 00:12:18 +0530

    practice 11 - create button dynamically

---
+++ JAVASCRIPT PRACTICE/practice 11/index.js
@@ -0,0 +1,8 @@
+let container = document.getElementById("container");
+
+let button = document.createElement("button");
+button.textContent = "Click me";
+button.style.height = "100px";
+button.style.width = "100px";
+button.style.background = "red";
+container.appendChild(button);
