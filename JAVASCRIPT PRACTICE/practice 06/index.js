18d09aa6ae5cb55748cd77c0ccee6f2986733f77
Viraj <virajdomadia32@gmail.com>
2024-09-21 23:05:59 +0530

    Practice 6 - Check if even or odd

---
+++ JAVASCRIPT PRACTICE/practice 06/index.js
@@ -0,0 +1,13 @@
+function checkEvenorOdd(n) {
+  if (n % 2 == 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+console.log(checkEvenorOdd(11));
+console.log(checkEvenorOdd(12));
+console.log(checkEvenorOdd(13));
+console.log(checkEvenorOdd(15));
+console.log(checkEvenorOdd(14));
