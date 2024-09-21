7e393187725b8e060b5b7da8c44371d0e4219501
Viraj <virajdomadia32@gmail.com>
2024-09-21 23:15:13 +0530

    Practice 7 - check if year is leap year or not

---
+++ JAVASCRIPT PRACTICE/practice 07/index.js
@@ -0,0 +1,13 @@
+function checkLeapYear(y) {
+  if (y % 4 == 0) {
+    console.log(y + " is a leap year");
+  } else {
+    console.log(y + " is not a leap year");
+  }
+}
+
+checkLeapYear(2000);
+checkLeapYear(2001);
+checkLeapYear(2002);
+checkLeapYear(2003);
+checkLeapYear(2004);
