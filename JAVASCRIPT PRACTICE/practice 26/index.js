7c74aac716eb15c14bca18d27af9931b9c3ce107
Viraj <virajdomadia32@gmail.com>
2024-09-25 23:07:13 +0530

    javascript practice

---
+++ JAVASCRIPT PRACTICE/practice 26/index.js
@@ -0,0 +1,21 @@
+function makeAPICall(url) {
+  return fetch(url).then((Response) => Response.json());
+}
+
+function handleData(data) {
+  console.log("processed Data", data);
+}
+
+function main() {
+  const apiUrl1 = "https://jsonplaceholder.typicode.com/posts/1";
+  const apiUrl2 = "https://jsonplaceholder.typicode.com/posts/2";
+
+  makeAPICall(apiUrl1)
+    .then(handleData)
+    .catch((err) => console.log("error"));
+  makeAPICall(apiUrl2)
+    .then(handleData)
+    .catch((err) => console.log("error"));
+}
+
+main();
