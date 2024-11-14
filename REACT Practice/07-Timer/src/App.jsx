e9830f8e7ec61b773f0604e9403be946281848c2
Viraj <virajdomadia32@gmail.com>
2024-11-14 22:39:44 +0530

    React 07-Timer

---
+++ REACT Practice/07-Timer/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useEffect } from "react";
+
+function App() {
+  const [time, setTime] = useState(60);
+
+  useEffect(() => {
+    if (time > 0) {
+      const timer = setTimeout(() => setTime(time - 1), 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [time]);
+
+  return (
+    <>
+      <h1>Time Left: {time} sec</h1>
+    </>
+  );
+}
+
+export default App;
